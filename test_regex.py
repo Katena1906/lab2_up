031df825aa7efabb177e5521326c7861123defae
Katena <katuskamakarona60@gmail.com>
2024-11-29 02:36:30 +0300

    add test class for f_i_f

--- test_regex.py
+++ test_regex.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, mock_open
+import os
 import requests
 from main import find_credit_card_numbers, find_in_file, find_in_input_text, find_in_site
 
@@ -11,7 +11,7 @@ class TestFindCreditCardNumbers(unittest.TestCase):
         self.assertEqual(find_credit_card_numbers(text), expected)
 
     def test_find_no_credit_card_numbers(self):
-        text = "There are no card numbers here!"
+        text = "My ID in Genshin 71-72-73-714"
         expected = []
         self.assertEqual(find_credit_card_numbers(text), expected)
 
@@ -19,3 +19,23 @@ class TestFindCreditCardNumbers(unittest.TestCase):
         text = "Card numbers: 1234567898765432, 1234-5678-1234-5678, 1111 2222 3333 4444."
         expected = ['1234567898765432', '1234-5678-1234-5678', '1111 2222 3333 4444']
         self.assertEqual(find_credit_card_numbers(text), expected)
+
+class TestFindInFile(unittest.TestCase):
+    def test_find_in_file_valid(self):
+        temp = "test.txt"
+        with open(temp, "w", encoding="utf-8") as f:
+            f.write("1222 5585 8585 9494\n4747 4884 4844 3373")
+        expected =['1222 5585 8585 9494','4747 4884 4844 3373']
+        self.assertEqual(find_in_file(temp), expected)
+        os.remove(temp)
+
+    def test_find_in_file_not_found(self):
+        self.assertEqual(find_in_file("non_existent_file.txt"), [])
+
+    def test_find_in_file_invalid_input(self):
+        with self.assertRaises(TypeError):
+            find_in_file(12345)
+
+
+
+
